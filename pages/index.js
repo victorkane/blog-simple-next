e5ce14dbb2abef26f88654d2a1b9295f8ab618c1
victorkane <victorkane@gmail.com>
2017-01-26 16:10:55 -0500

    static version of home page for #2

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Router from 'next/router'
+
+export default class extends React.Component {
+  static getInitialProps () {
+    return {
+      posts: new Array(15).fill({title: 'This is a post'}).map((v, k) => v.title = v.title + ' ' + k)
+    }
+  }
+
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    let clave = 0;
+    return(
+      <div className="posts">
+        { this.props.posts.map(post => (
+            <div className="post" key={clave = clave + 1}><p>{post}</p></div>
+        ))}
+      </div>
+    )
+  }
+}
